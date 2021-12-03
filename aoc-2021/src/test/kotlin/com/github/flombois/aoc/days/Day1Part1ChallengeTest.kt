18aad61eb3a3e2215c49c7039726395be8757e1a
Florent <78081121+florent-lombois@users.noreply.github.com>
2021-12-03 21:02:45 +0100

    Add test for day 1 part 1

---
+++ aoc-2021/src/test/kotlin/com/github/flombois/aoc/days/Day1Part1ChallengeTest.kt
@@ -0,0 +1,12 @@
+package com.github.flombois.aoc.days
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class Day1Part1ChallengeTest {
+
+    @Test
+    fun test() {
+        assertEquals(7, Integer.valueOf(Day1Part1Challenge().computeSolution()))
+    }
+}
